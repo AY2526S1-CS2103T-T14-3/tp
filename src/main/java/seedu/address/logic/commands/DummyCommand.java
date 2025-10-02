5ecc7f5352758ae72712153512c60abcdbe61f2a
louist3ng <tenglouistjj@gmail.com>
2025-10-02 15:24:38 +0800

    Add Dummy Command

---
+++ src/main/java/seedu/address/logic/commands/DummyCommand.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.commands;
+
+public class DummyCommand {
+    // This is a dummy command class for testing purposesq
+    public DummyCommand() {
+        // Constructor
+    }   
+}

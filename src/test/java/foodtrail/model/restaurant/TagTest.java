aa976cd9f391d025ee48dec5af87dee82066d3ac
ChewyMochi <austinchen237@gmail.com>
2025-10-22 17:14:01 +0800

    Add equals method test cases

--- src/test/java/foodtrail/model/restaurant/TagTest.java
+++ src/test/java/foodtrail/model/restaurant/TagTest.java
@@ -1,6 +1,8 @@
 package foodtrail.model.restaurant;
 
 import static foodtrail.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,7 +27,24 @@ public class TagTest {
 
     @Test
     public void equals() {
+        Tag tag = new Tag("fastfood");
 
+        // same object -> returns true
+        assertTrue(tag.equals(tag));
+
+        // same values -> returns true
+        Tag tagCopy = new Tag("fastfood");
+        assertTrue(tag.equals(tagCopy));
+
+        // different types -> returns false
+        assertFalse(tag.equals(1));
+
+        // null -> returns false
+        assertFalse(tag.equals(null));
+
+        // different tag -> returns false
+        Tag differentTag = new Tag("western");
+        assertFalse(tag.equals(differentTag));
     }
 
 }

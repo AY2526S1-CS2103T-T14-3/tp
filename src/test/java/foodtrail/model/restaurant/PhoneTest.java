b9a77ede5955d557ba65aa94a3cd7e9714a6cfaf
ChewyMochi <austinchen237@gmail.com>
2025-10-22 17:03:26 +0800

    Update test cases

--- src/test/java/foodtrail/model/restaurant/PhoneTest.java
+++ src/test/java/foodtrail/model/restaurant/PhoneTest.java
@@ -27,15 +27,16 @@ public class PhoneTest {
         // invalid phone numbers
         assertFalse(Phone.isValidPhone("")); // empty string
         assertFalse(Phone.isValidPhone(" ")); // spaces only
-        assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
+        assertFalse(Phone.isValidPhone("91")); // less than 8 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
+        assertFalse(Phone.isValidPhone("78128381")); // not starting with 6,8 or 9
 
         // valid phone numbers
         assertTrue(Phone.isValidPhone("85627513"));
         assertTrue(Phone.isValidPhone("93121534"));
-        assertTrue(Phone.isValidPhone("63451179")); // long phone numbers
+        assertTrue(Phone.isValidPhone("63451179"));
     }
 
     @Test

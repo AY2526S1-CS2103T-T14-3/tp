5203aa3579a4527658c5ef9c982e8d4fa39683dd
louist3ng <tenglouistjj@gmail.com>
2025-10-23 11:39:03 +0800

    Edit UniqueRestaurantList that was affected when resolving conflicts

--- src/test/java/foodtrail/model/restaurant/UniqueRestaurantListTest.java
+++ src/test/java/foodtrail/model/restaurant/UniqueRestaurantListTest.java
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;

7590a03ed8775a8aa867736708b43b9592cb4eab
Chanism0808 <chanjustin999@gmail.com>
2025-10-28 14:56:03 +0800

    Edit UntagCommandParser

--- src/main/java/foodtrail/logic/parser/UntagCommandParser.java
+++ src/main/java/foodtrail/logic/parser/UntagCommandParser.java
@@ -4,22 +4,22 @@ import static foodtrail.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static foodtrail.logic.parser.CliSyntax.PREFIX_TAG;
 import static java.util.Objects.requireNonNull;
 
+import java.util.Collection;
 import java.util.Set;
 
 import foodtrail.commons.core.index.Index;
-import foodtrail.logic.commands.TagCommand;
 import foodtrail.logic.commands.UntagCommand;
 import foodtrail.logic.parser.exceptions.ParseException;
 import foodtrail.model.restaurant.Tag;
 
 /**
- * Parses input arguments and creates a new TagCommand object
+ * Parses input arguments and creates a new UntagCommand object
  */
 public class UntagCommandParser implements Parser<UntagCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the TagCommand
-     * and returns a TagCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the UntagCommand
+     * and returns an UntagCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public UntagCommand parse(String args) throws ParseException {
@@ -30,14 +30,22 @@ public class UntagCommandParser implements Parser<UntagCommand> {
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UntagCommand.MESSAGE_USAGE), pe);
         }
 
-        if (!argMultimap.getValue(PREFIX_TAG).isPresent()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE));
+        Collection<String> tagNames = argMultimap.getAllValues(PREFIX_TAG);
+
+        // Handles `untag 1` - no tag prefix provided
+        if (tagNames.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UntagCommand.MESSAGE_USAGE));
         }
 
-        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+        // Handles `untag 1 t/` - tag prefix is present but empty
+        if (tagNames.stream().anyMatch(String::isEmpty)) {
+            throw new ParseException(UntagCommand.MESSAGE_EMPTY_TAG);
+        }
+
+        Set<Tag> tagList = ParserUtil.parseTags(tagNames);
 
         return new UntagCommand(index, tagList);
     }

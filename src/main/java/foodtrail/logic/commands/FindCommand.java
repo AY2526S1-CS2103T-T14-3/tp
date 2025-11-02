c542faaa1212599758fdaad8ba6c436d88420efc
christopherchong <chrstchong@gmail.com>
2025-11-03 02:16:55 +0800

    Update output of Find command to be more descriptive

--- src/main/java/foodtrail/logic/commands/FindCommand.java
+++ src/main/java/foodtrail/logic/commands/FindCommand.java
@@ -3,7 +3,6 @@ package foodtrail.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import foodtrail.commons.util.ToStringBuilder;
-import foodtrail.logic.Messages;
 import foodtrail.model.Model;
 import foodtrail.model.restaurant.RestaurantContainsKeywordsPredicate;
 
@@ -21,10 +20,17 @@ public class FindCommand extends Command {
             + "Parameters: KEYWORD\n"
             + "Example: " + COMMAND_WORD + " kfc";
 
-    private final RestaurantContainsKeywordsPredicate predicate;
+    public static final String MESSAGE_FIND_SUCCESS = "%1$d restaurants listed with the keyword '%2$s'";
 
-    public FindCommand(RestaurantContainsKeywordsPredicate predicate) {
+    private final RestaurantContainsKeywordsPredicate predicate;
+    private final String keyword;
+
+    /**
+     * Creates a FindCommand to find the specified {@code RestaurantContainsKeywordsPredicate} and keyword.
+     */
+    public FindCommand(RestaurantContainsKeywordsPredicate predicate, String keyword) {
         this.predicate = predicate;
+        this.keyword = keyword;
     }
 
     @Override
@@ -32,7 +38,7 @@ public class FindCommand extends Command {
         requireNonNull(model);
         model.updateFilteredRestaurantList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_RESTAURANTS_LISTED_OVERVIEW, model.getFilteredRestaurantList().size()));
+                String.format(MESSAGE_FIND_SUCCESS, model.getFilteredRestaurantList().size(), keyword));
     }
 
     @Override
@@ -47,13 +53,15 @@ public class FindCommand extends Command {
         }
 
         FindCommand otherFindCommand = (FindCommand) other;
-        return predicate.equals(otherFindCommand.predicate);
+        return predicate.equals(otherFindCommand.predicate)
+                && keyword.equals(otherFindCommand.keyword);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
                 .add("predicate", predicate)
+                .add("keyword", keyword)
                 .toString();
     }
 }
